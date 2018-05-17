0f8c3e0762482e353fdb3cee3a63b61412ffd404
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-05-17 20:00:21 +0200

    return UI using stateless function

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react'
 
+// if all my component has is a render method so we 
+// could use a Stateless function component or a basic function that takes props as an argument
 
-class ListContacts extends Component {
-  render() {
-    return (
-      <ol className='contact-list'>
-        {
-          this.props.contacts.map( (contact) => (
-            <li key={contact.id} className='contact-list-item'>
-              <div className='contact-avatar' style={{
-                background: `url(${contact.avatarURL})`
-              }}/>
-              <div className='contact-details'>
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button className='contact-remove'>
-                Remove
-              </button>
-            </li>
-          ))}
-      </ol>
-    )
-  }
+function ListContacts (props){
+  return(
+    <ol className='contact-list'>
+      {
+        props.contacts.map( (contact) => (
+          <li key={contact.id} className='contact-list-item'>
+            <div className='contact-avatar' style={{
+              background: `url(${contact.avatarURL})`
+            }}/>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className='contact-remove'>
+              Remove
+            </button>
+          </li>
+        ))}
+    </ol>
+  )
 }
 
 // export this so we can import it at app.js file
