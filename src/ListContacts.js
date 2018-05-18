2b3ff09f7823a627487e4ef10080e848cfc98ed0
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-05-18 14:12:23 +0200

    turn the function into class and add input field and the state and the query Update

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,35 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-// if all my component has is a render method so we 
-// could use a Stateless function component or a basic function that takes props as an argument
-
-function ListContacts (props){
-  return(
-    <ol className='contact-list'>
-      {
-        props.contacts.map( (contact) => (
-          <li key={contact.id} className='contact-list-item'>
-            <div className='contact-avatar' style={{
-              background: `url(${contact.avatarURL})`
-            }}/>
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-              Remove
-            </button>
-          </li>
-        ))}
-    </ol>
-  )
-}
-
-// add prop type module to strict the user with the data type we want to pass
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+// now we want to add state t our component so we make it class not function
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+  state = {
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState({query: query.trim() })
+  }
+  render (){
+    return(
+      <div className='list-contacts'>
+        {JSON.stringify(this.state)}
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={this.state.query}
+            onChange ={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className='contact-list'>
+          {
+            this.props.contacts.map( (contact) => (
+              <li key={contact.id} className='contact-list-item'>
+                <div className='contact-avatar' style={{
+                  background: `url(${contact.avatarURL})`
+                }}/>
+                <div className='contact-details'>
+                  <p>{contact.name}</p>
+                  <p>{contact.email}</p>
+                </div>
+                <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                  Remove
+                </button>
+              </li>
+            ))}
+        </ol>
+      </div>  
+    )
+  }
 }
 
 // export this so we can import it at app.js file
