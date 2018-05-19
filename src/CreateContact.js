35fa149fe29a9abbce8fb97b8a6f03d012f372b0
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-05-20 00:26:03 +0200

    serialize data from the form and send it to method oncreatecontact

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+  handleSubmit = (e) => {
+    /*to prevent browser from handling sumbit it self*/
+    e.preventDefault()
+    /*serialize form data ourselves, and get object hold the form data*/
+    const values = serializeForm(e.target, {hash: true})
+    /*console.log(values)*/
+    if(this.props.onCreateContact)
+      this.props.onCreateContact(values)
+  }
   render() {
     return(
       <div>
         <Link to='/' className='close-create-contact'>
           Close
         </Link>
-        <form className='create-contact-form'>
+        <form onSubmit={this.handleSubmit} className='create-contact-form'>
           <ImageInput
             className='create-contact-avatar-input'
             name='avatarURL'
