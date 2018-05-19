26bd9d49900636aa2f16209e4eecd7c04fdb189c
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-05-19 18:21:25 +0200

    show number of contacts shown on filter

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,9 @@ class ListContacts extends Component {
   updateQuery = (query) => {
     this.setState({query: query.trim() })
   }
+  clearQuery = () => {
+    this.setState({query: ''})
+  }
   render (){
     /*to make the code a little bit cleaner use derstructure from ES6*/
     const { contacts, onDeleteContact } = this.props
@@ -44,6 +47,13 @@ class ListContacts extends Component {
             onChange ={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+        
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show All</button>
+          </div>
+        )}
         <ol className='contact-list'>
           {
             showingContacts.map( (contact) => (
